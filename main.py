bf719e79641118da5006ae8b7903ec76598d2400
andresclaroavocado <andres.claro@avocadoblock.com>
2025-08-04 14:39:30 -0700

    update api

--- main.py
+++ main.py
@@ -80,6 +80,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000", 
         "http://127.0.0.1:3000",
+        "https://project-launcher.vercel.app",
         "https://*.vercel.app",
         "https://*.vercel.com"
     ],
